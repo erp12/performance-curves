2338558b13f244be7b192167be3ab7f8b14fd8c4
erp12 <eddie.pantridge@gmail.com>
2020-09-29 15:08:41 -0400

    Adds doc website generation

--- performance_curves/__init__.py
+++ performance_curves/__init__.py
@@ -1,1 +1,16 @@
 """Write me."""
+from typing import List
+
+
+def foo(i: int, s: str) -> List[str]:
+    """A simple function
+
+    Arguments:
+        i: Some number
+        s: Some test
+
+    Return:
+        A list of `s` with `i` elements.
+
+    """
+    return [s] * i
