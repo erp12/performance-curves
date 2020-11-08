c73143cdf2e050bf2e31b1eea2cf1a2ee2c092fb
Tam Tran-The <tam.tranthe@enovant.com>
2020-11-08 16:48:57 -0500

    add custom metric classes for commonly used ones, i.e. precision, recall, f1 score and accuracy

--- performance_curves/metric.py
+++ performance_curves/metric.py
@@ -1,8 +1,9 @@
 from typing import Callable
 import numpy as np
 
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
-# TODO: write custom metrics for precision, recall, F1 and accuracy
+
 class Metric:
     def __init__(self,
                  name: str,
@@ -11,3 +12,23 @@ class Metric:
         self.name = name
         self.func = func
         self.should_minimize = should_minimize
+
+
+class PrecisionMetric(Metric):
+    def __init__(self, name='Precision', func=precision_score, should_minimize=False):
+        super().__init__(name, func, should_minimize)
+
+
+class RecallMetric(Metric):
+    def __init__(self, name='Recall', func=recall_score, should_minimize=False):
+        super().__init__(name, func, should_minimize)
+
+
+class F1Metric(Metric):
+    def __init__(self, name='F1 Score', func=f1_score, should_minimize=False):
+        super().__init__(name, func, should_minimize)
+
+
+class AccuracyMetric(Metric):
+    def __init__(self, name='Accuracy', func=accuracy_score, should_minimize=False):
+        super().__init__(name, func, should_minimize)
