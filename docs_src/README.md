bc944cdb28508e9b0ff85793cc3e492ce668a2c5
erp12 <eddie.pantridge@gmail.com>
2021-07-03 17:12:05 -0400

    Switch to poetry for deps management

--- docs_src/README.md
+++ docs_src/README.md
@@ -1,6 +1,6 @@
 # Performance Curves
 
-## Rationale 
+## Rationale
 
 ## Getting Started
 
@@ -21,3 +21,19 @@ of Python using the one of the variants.
 ```
 tox -e test-py36  # or test-py37 or test-py38
 ```
+
+### TODO:
+- [ ] Rewrite developer guide
+- [ ] Contributing guide
+- [ ] Release notes
+- [ ] Examples
+- [ ] Create performance curve bundle (1 test set / 1 metric / multiple models)
+- [ ] Method to select best model for a given objective
+- [ ] Linter in unit tests
+
+### Questions
+- Do we want to allow multiple metrics in the same `performance_curve` function?
+    - Pro: save computation time, since given the same model, no matter what the metric is, we still use the same `y_pred` and `y_score`
+- Do we want to allow multiple models in the same `performance_curve` function?
+    - Con: doesn't save any computation time (cause there's nothing to be reused across models)
+  
--- performance_curves/performance_curve_bundle.py
+++ performance_curves/performance_curve_bundle.py
@@ -1,10 +1,16 @@
-from typing import Mapping
-from performance_curves.performance_curve import *
+from typing import Mapping, Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from performance_curves.metric import MetricInfo
+from performance_curves.performance_curve import PerformanceCurve, RandomPerformanceCurve, PerfectPerformanceCurve
 
 
 class PerformanceCurveBundle:
     """A bundle includes multiple performance curves that represent multiple methods (e.g. ranking systems, or ML models)
     applied to the same test data and evaluated under the same metric"""
+
     def __init__(self,
                  y_true: np.ndarray,
                  method_scores: Mapping[str, np.ndarray],
@@ -25,7 +31,8 @@ class PerformanceCurveBundle:
         self.perfect_curve = None
 
         for method_name, y_score in self.method_scores.items():
-            self.curves[method_name] = PerformanceCurve(self.y_true, y_score, self.metric, self.num_bins, self.order_descending)
+            self.curves[method_name] = PerformanceCurve(self.y_true, y_score, self.metric, self.num_bins,
+                                                        self.order_descending)
 
         if make_perfect:
             self.perfect_curve = PerfectPerformanceCurve(self.y_true, self.metric, self.num_bins)
@@ -36,6 +43,7 @@ class PerformanceCurveBundle:
         def create_x_axis_values(raw_counts: np.ndarray):
             x_values = raw_counts / raw_counts[-1] if percentage_x else raw_counts
             return x_values
+
         fig, ax = plt.subplots()
         for curve_name, curve in self.curves.items():
             ax.plot(create_x_axis_values(curve.case_counts), curve.performance_values, label=curve_name)
